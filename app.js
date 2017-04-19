56ede8c8a14f5c196dae6094d1f5f288848b8e95
gkostin1966 <gkostin@umich.edu>
2017-04-19 12:29:36 -0400

    .gitignore

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 (function() {
   'use strict';
   angular.module('myFirstApp', [])
-    .controller('MyFirstController', function($scope) {
-      $scope.name = "Greg"
-      $scope.sayHello = function() {
-        return "Hello Coursera!";
-      };
-    });
+    .controller('MyFirstController', MyFirstController);
+
+  function MyFirstController($scope) {
+    $scope.name = "Greg"
+    $scope.total = calculateNumericForString($scope.name);
+
+    $scope.displayNumeric = function() {
+      $scope.total = calculateNumericForString($scope.name);
+    };
+
+    function calculateNumericForString(string) {
+      var totalStringValue = 0;
+      for (var i = 0; i < string.length; i++) {
+        totalStringValue += string.charCodeAt(i);
+      }
+      return totalStringValue;
+    }
+  }
 })();
