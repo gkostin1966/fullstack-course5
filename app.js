bc7373b095954fd844ab6673610f97490bcdbe3f
gkostin1966 <gkostin@umich.edu>
2017-04-19 13:05:54 -0400

    minification

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 (function() {
   'use strict';
   angular.module('myFirstApp', [])
-    .controller('MyFirstController', ['$scope', '$filter', '$injector', function($scope, $filter, $injector) {
-      $scope.name = "Greg";
+    .controller('MyFirstController', MyFirstController);
+
+  MyFirstController.$inject = ['$scope', '$filter', '$injector'];
+
+  function MyFirstController($scope, $filter, $injector) {
+    $scope.name = "Greg";
+    $scope.total = calculateNumericForString($scope.name);
+
+    $scope.upper = function() {
+      var upCase = $filter('uppercase');
+      $scope.name = upCase($scope.name);
+    };
+
+    $scope.displayNumeric = function() {
       $scope.total = calculateNumericForString($scope.name);
+    };
 
-      $scope.upper = function() {
-        var upCase = $filter('uppercase');
-        $scope.name = upCase($scope.name);
-      };
-
-      $scope.displayNumeric = function() {
-        $scope.total = calculateNumericForString($scope.name);
-      };
-
-      function calculateNumericForString(string) {
-        var totalStringValue = 0;
-        for (var i = 0; i < string.length; i++) {
-          totalStringValue += string.charCodeAt(i);
-        }
-        return totalStringValue;
+    function calculateNumericForString(string) {
+      var totalStringValue = 0;
+      for (var i = 0; i < string.length; i++) {
+        totalStringValue += string.charCodeAt(i);
       }
+      return totalStringValue;
+    }
 
-      // console.log($injector.annotate(this));
-    }]);
+    console.log($injector.annotate(MyFirstController));
+  }
 })();
