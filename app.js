75368b83cd8bfb30444c1d409fd2ecc1ee218231
gkostin1966 <gkostin@umich.edu>
2017-04-19 13:01:18 -0400

    uglify

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
 (function() {
   'use strict';
   angular.module('myFirstApp', [])
-    .controller('MyFirstController', ['$scope', '$filter', '$injector', MyFirstController]);
-
-  function MyFirstController($scope, $filter, $injector) {
-    $scope.name = "Greg"
-    $scope.total = calculateNumericForString($scope.name);
-
-    $scope.upper = function() {
-      var upCase = $filter('uppercase');
-      $scope.name = upCase($scope.name);
-    };
-
-    $scope.displayNumeric = function() {
+    .controller('MyFirstController', ['$scope', '$filter', '$injector', function($scope, $filter, $injector) {
+      $scope.name = "Greg";
       $scope.total = calculateNumericForString($scope.name);
-    };
 
-    function calculateNumericForString(string) {
-      var totalStringValue = 0;
-      for (var i = 0; i < string.length; i++) {
-        totalStringValue += string.charCodeAt(i);
+      $scope.upper = function() {
+        var upCase = $filter('uppercase');
+        $scope.name = upCase($scope.name);
+      };
+
+      $scope.displayNumeric = function() {
+        $scope.total = calculateNumericForString($scope.name);
+      };
+
+      function calculateNumericForString(string) {
+        var totalStringValue = 0;
+        for (var i = 0; i < string.length; i++) {
+          totalStringValue += string.charCodeAt(i);
+        }
+        return totalStringValue;
       }
-      return totalStringValue;
-    }
 
-    console.log($injector.annotate(MyFirstController));
-  }
+      // console.log($injector.annotate(this));
+    }]);
 })();
