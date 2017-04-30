2c6f23ff03169bad0445961793248ba4bf5591e5
gkostin1966 <gkostin@umich.edu>
2017-04-30 14:36:48 -0400

    menuapp

---
+++ module4-solution/src/menudata.service.js
@@ -0,0 +1,3 @@
+/**
+ * Created by gkostin on 4/30/17.
+ */
--- module4-solution/src/shoppinglist/shoppinglist.module.js
+++ module4-solution/src/shoppinglist/shoppinglist.module.js
@@ -1,6 +1,6 @@
 (function () {
 'use strict';
 
-angular.module('ShoppingList', ['ui.router', 'Spinner']);
+angular.module('MenuApp', ['ui.router', 'Spinner']);
 
 })();
