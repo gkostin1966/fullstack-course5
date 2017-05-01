c79690f6a12b07f218766a985f00c26813bbc96c
gkostin1966 <gkostin@umich.edu>
2017-05-01 17:02:09 -0400

    davids-restaurant

--- module4-solution/src/menudata.service.js
+++ module4-solution/src/menudata.service.js
@@ -2,59 +2,32 @@
 'use strict';
 
 angular.module('data')
-.service('MenuDataService', MenuDataService);
+.service('MenuDataService', MenuDataService)
+.constant('ApiBasePath', 'https://davids-restaurant.herokuapp.com');
 
 
-MenuDataService.$inject = ['$q', '$timeout']
-function MenuDataService($q, $timeout) {
+MenuDataService.$inject = ['$http', 'ApiBasePath'];
+function MenuDataService($http, ApiBasePath) {
   var service = this;
 
-  // List of shopping items
-  var categories = [];
-
-  // Pre-populate a no cookie list
-  categories.push({
-    name: "Sugar",
-    quantity: "2 bags",
-    description: "Sugar used for baking delicious umm... baked goods."
-  });
-  categories.push({
-    name: "flour",
-    quantity: "1 bags",
-    description: "High quality wheat flour. Mix it with water, sugar, 2 raw eggs."
-  });
-  categories.push({
-    name: "Chocolate Chips",
-    quantity: "3 bags",
-    description: "Put these in the dough. No reason, really. Gotta store them somewhere!"
-  });
-
-  // Simulates call to server
-  // Returns a promise, NOT items array directly
   service.getAllCategories = function () {
-    var deferred = $q.defer();
-
-    // Wait 2 seconds before returning
-    $timeout(function () {
-      // deferred.reject(items);
-      deferred.resolve(categories);
-    }, 800);
-
-    return deferred.promise;
+    return $http({
+      method: "GET",
+      url: (ApiBasePath + "/categories.json")
+    })
+      .then(function (result) {
+        return result.data;
+      });
   };
 
-  // Simulates call to server
-  // Returns a promise, NOT items array directly
   service.getItemsForCategory = function (categoryShortName) {
-    var deferred = $q.defer();
-
-    // Wait 2 seconds before returning
-    $timeout(function () {
-      // deferred.reject(items);
-      deferred.resolve([categories[categoryShortName]]);
-    }, 800);
-
-    return deferred.promise;
+    return $http({
+      method: "GET",
+      url: (ApiBasePath + "/menu_items.json?category=" + categoryShortName)
+    })
+      .then(function (result) {
+        return result.data.menu_items;
+      });
   };
 }
 
